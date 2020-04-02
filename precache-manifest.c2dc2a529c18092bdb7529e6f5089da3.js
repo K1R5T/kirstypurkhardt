49b71cd6df2975892df7cc36be374d8a9a4d8538
kirsty <kirstypurkhardt@gmail.com>
2020-04-02 16:06:17 +0100

    Updates

---
+++ precache-manifest.c2dc2a529c18092bdb7529e6f5089da3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e03ca2098fcd3c9ebd192b2a6dfc7c6",
+    "url": "/kirstypurkhardt/index.html"
+  },
+  {
+    "revision": "dcc9a285f971706f7650",
+    "url": "/kirstypurkhardt/static/css/main.58e493af.chunk.css"
+  },
+  {
+    "revision": "4b2b9a8c8e1e35aa9101",
+    "url": "/kirstypurkhardt/static/js/2.a3df654a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kirstypurkhardt/static/js/2.a3df654a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc9a285f971706f7650",
+    "url": "/kirstypurkhardt/static/js/main.395f3904.chunk.js"
+  },
+  {
+    "revision": "5c42efa2be846b697331",
+    "url": "/kirstypurkhardt/static/js/runtime-main.a4190ded.js"
+  },
+  {
+    "revision": "2060212b3d53a14e27f63901a9be8a00",
+    "url": "/kirstypurkhardt/static/media/me.2060212b.jpeg"
+  },
+  {
+    "revision": "0a13c95d939369c4e5cdaa792a84f0d5",
+    "url": "/kirstypurkhardt/static/media/projectsbackground.0a13c95d.jpg"
+  }
+]);
